3d2316c1b28a895c1c968fad02e61f26b8fcdfa7
Toby Such <such@adobe.com>
2024-05-29 15:42:50 +0100

    Add contact widget code

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,7 +1,26 @@
 // eslint-disable-next-line import/no-cycle
-import { sampleRUM } from './aem.js';
+import { sampleRUM, loadScript, loadCSS } from './aem.js';
 
 // Core Web Vitals RUM collection
 sampleRUM('cwv');
 
-// add more delayed functionality here
+// Chat
+window.EDU = {
+  Configs: {
+    chatApiURL: '/genesys/2/chat/Chat',
+    chatCallBackURL: '/genesys/1/service/callback',
+    chatCometURL: '/genesys/cometd',
+    chatDataURL: '/genesys/2/chat/Chat',
+    chatDomain: 'https://ucpgms.phoenix.edu',
+    chatGWEHttpEndPoint: 'http://gwe.phoenix.edu',
+    chatGWEHttpsEndPoint: 'https://gwe.phoenix.edu',
+
+  },
+  chatConfig: {
+    chatService: '',
+    callBackUrl: '',
+    chatClientlibHashCode: '1956f4d4e84fb208cfba5940203678b7',
+  },
+};
+loadScript('/clientlibs/clientlib-chat.js');
+loadCSS('/clientlibs/clientlib-chat.css');
