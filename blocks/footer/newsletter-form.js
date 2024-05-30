090d15a6d65e5f7162f2756cd5257dbe60fd7e7e
Varun Bhatia <varu@adobe.com>
2024-05-31 03:05:42 +0530

    dummy change

--- blocks/footer/newsletter-form.js
+++ blocks/footer/newsletter-form.js
@@ -1,16 +1,17 @@
 export function loadNewsletterForm() {
-  var scrolledPercent;
-  var loadingflag = true;
+  let scrolledPercent;
+  let loadingflag = true;
 
   document.addEventListener('scroll', function () {
-    let scrollPercentage = window.scrollY / (document.documentElement.scrollHeight - document.documentElement.clientHeight) * 100;
+    // eslint-disable-next-line max-len
+    const scrollPercentage = (window.scrollY / (document.documentElement.scrollHeight - document.documentElement.clientHeight)) * 100;
     scrolledPercent = Math.round(scrollPercentage);
 
     if (scrolledPercent > 70 && loadingflag) {
       loadingflag = false;
       document.querySelector('.newsletter-form.block').innerHTML = '<div id="edu-footer-widget"></div>';
 
-      var formscript = document.createElement('script');
+      let formscript = document.createElement('script');
       formscript.type = 'text/javascript';
       formscript.src = 'https://www.phoenix.edu/request/static/includeWidget.js';
       formscript.setAttribute('data-mode', 'edu-footer-widget');
