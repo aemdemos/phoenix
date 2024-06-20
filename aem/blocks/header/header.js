af5e2a00b9a1c68d76917b3f90720df87b216da3
Divyansh Pratap <dipratap@adobe.com>
2024-06-20 14:05:15 +0530

    add header strip

--- aem/blocks/header/header.js
+++ aem/blocks/header/header.js
@@ -1,5 +1,4 @@
-// import { a, div, p } from '../../scripts/dom-helpers.js';
-import { loadScript } from '../../scripts/aem.js';
+import { a, div, p } from '../../scripts/dom-helpers.js';
 
 /**
  * loads and decorates the header, mainly the nav
@@ -13,6 +12,13 @@ export default async function decorate(block) {
     const headerDiv = document.createElement('div');
     headerDiv.innerHTML = await resp.text();
     body.append(headerDiv);
+    const headerContainer = headerDiv.querySelector('.cmp-header');
+    headerContainer.classList.add('alertWindowMargin');
+    const announcementBar = div(
+      { id: 'announcementBar', class: 'container' },
+      p({ id: 'alertCopy' }, 'Request a ', a({ class: 'alertWindowLink', href: '#' }, 'Free Scholarships and Savings Guide')),
+    );
+    headerContainer.prepend(announcementBar);
     headerDiv.querySelectorAll('a').forEach((anchor) => {
       // eslint-disable-next-line no-script-url
       if (anchor.getAttribute('href') === 'javascript:void(0)') {
